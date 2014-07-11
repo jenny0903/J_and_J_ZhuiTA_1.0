ffb27f16a13209ff1bd5194faf89be8a629bb501
JaneChen <chentianyin@163.com>
2014-07-11 16:12:30 +0800

    orders js 3

--- pickupadmin/static/js/exchange_orders.js
+++ pickupadmin/static/js/exchange_orders.js
@@ -4,11 +4,11 @@ var exchange_orders = {
 	max_page : 1,
 	tpl : [
 		'<li data={id}>',
-			'<span class="name">{name}</span>',
-			'<span class="product">{product}</span>',
-			'<span class="address">{address}</span>',
+			'<span class="name"><a title="{name2}">{name}</a></span>',
+			'<span class="product"><a title="{product2}">{product}</a></span>',
+			'<span class="address"><a title="{address2}">{address}</a></span>',
 			'<span class="phone">{phone}</span>',
-			'<span class="status">{status}</span>',
+			'<span class="status"><a class="manage_order" data={state} href="javascript:;">{status}</a></span>',
 		'</li>'
 	].join(''),
 	list_orders : function(){
@@ -32,7 +32,7 @@ var exchange_orders = {
 			success: function(data){
 				window.parent.$("#J_loading_wrap").hide();
 				var exchange_orders_list = data.orders;
-				var exchange_user_name, exchange_product_name, exchange_address, exchange_status,exchange_phone;
+				var exchange_user_name, exchange_product_name, exchange_address,exchange_state ,exchange_status,exchange_phone;
 				exchange_orders.max_page = Math.ceil(data.total/exchange_orders.page_items);
 				for(exchange_orders_i in exchange_orders_list){
 					exchange_user_id = exchange_orders_list[exchange_orders_i].id;
@@ -52,11 +52,11 @@ var exchange_orders = {
 					if(exchange_address == ''){
 						exchange_address == 'null';
 					}
-					exchange_status = exchange_orders_list[exchange_orders_i].status;
+					exchange_state = exchange_orders_list[exchange_orders_i].status;
 					
-					switch(exchange_status){
+					switch(exchange_state){
 						case 0:
-							exchange_status = '<a class="manage_order" href="javascript:;">未处理</a>';
+							exchange_status = '未处理';
 							break;
 						case 1:
 							exchange_status = '已处理';
@@ -66,7 +66,7 @@ var exchange_orders = {
 							break;
 					}
 					
-					$('#J_exchange_orders').append(exchange_orders.tpl.replace('{id}',exchange_user_id).replace('{name}',exchange_user_name).replace('{product}',exchange_product_name).replace('{address}',exchange_address).replace('{status}',exchange_status).replace('{phone}',exchange_phone));
+					$('#J_exchange_orders').append(exchange_orders.tpl.replace('{id}',exchange_user_id).replace('{name}',exchange_user_name).replace('{product}',exchange_product_name).replace('{address}',exchange_address).replace('{name2}',exchange_user_name).replace('{product2}',exchange_product_name).replace('{address2}',exchange_address).replace('{status}',exchange_status).replace('{phone}',exchange_phone));
 				}
 				
 				for(var i_page=1;i_page<=exchange_orders.max_page;i_page++){
@@ -83,29 +83,38 @@ var exchange_orders = {
 	},
 	manage_orders : function(order_li){
 		var order_id = order_li.attr('data');
+		var type = order_li.find('.manage_order')attr('data');
+		var new_status;
+		if( type == 1 || type == '1' ){
+			new_status = 0;
+		}
 		$.ajax({
 			type: "POST",
 			url: ajax_main_path+'libs/controller/exchnage_order.php',
-			data : 'id='+order_id,
+			data : 'id='+order_id+'&status='+new_status,
 			dataType:"JSON",
 			success: function(data){
 				window.parent.$("#J_loading_wrap").hide();
 				if(data == 1 || data == '1'){
-					order_li.find('.status').text('已处理');
+					if( new_status == 1 ){
+						order_li.find('.manage_order').text('已处理');
+					}else{
+						order_li.find('.manage_order').text('未处理');
+					}
 					if(window.parent.$("#J_alert_wrap").length==0){
 						window.parent.$('body').append(tpl.alert_box);
-						window.parent.$("#J_alert_wrap .alert_content").text('处理该订单成功！');
+						window.parent.$("#J_alert_wrap .alert_content").text('该订单状态修改成功！');
 					}else{
-						window.parent.$("#J_alert_wrap .alert_content").text('处理该订单成功！');
+						window.parent.$("#J_alert_wrap .alert_content").text('该订单状态修改成功！');
 						window.parent.$("#J_alert_wrap").show();							
 					}
 					setTimeout(hideAlert, 1000);
 				}else{
 					if(window.parent.$("#J_alert_wrap").length==0){
 						window.parent.$('body').append(tpl.alert_box);
-						window.parent.$("#J_alert_wrap .alert_content").text('处理该订单失败，请重试！');
+						window.parent.$("#J_alert_wrap .alert_content").text('该订单状态修改失败，请重试！');
 					}else{
-						window.parent.$("#J_alert_wrap .alert_content").text('处理该订单失败，请重试！');
+						window.parent.$("#J_alert_wrap .alert_content").text('该订单状态修改失败，请重试！');
 						window.parent.$("#J_alert_wrap").show();							
 					}
 					setTimeout(hideAlert, 1000);
@@ -126,10 +135,10 @@ $('.manage_order').die().live('click',function(){
 	var _this_li = $(this).parents('li');
 	if(window.parent.$("#J_confirm_wrap").length==0){
 		window.parent.$('body').append(tpl.confirm_box);
-		window.parent.$("#J_confirm_wrap .confirm_content").text('确定处理该订单吗？');
+		window.parent.$("#J_confirm_wrap .confirm_content").text('确定修改该订单状态吗？');
 		window.parent.$("#J_confirm_wrap").show();
 	}else{
-		window.parent.$("#J_confirm_wrap .confirm_content").text('确定处理该订单吗？');
+		window.parent.$("#J_confirm_wrap .confirm_content").text('确定修改该订单状态吗？');
 		window.parent.$("#J_confirm_wrap").show();
 	}
 	window.parent.$('#J_confirm_btn').die().live('click',function(){
