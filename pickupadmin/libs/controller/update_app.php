63ed1e8ae9ad9b2a336881a40ab4ffcbfe3ab8ab
jalc <jalc@yunio.com>
2014-11-03 12:16:21 +0800

    app_recommend

--- pickupadmin/libs/controller/update_app.php
+++ pickupadmin/libs/controller/update_app.php
@@ -1,6 +1,5 @@
 <?php
 include("../../config/config.php");
-/*
 if( isset($_POST['app_name']) && $_POST['app_name'] != '' ){
 	$app_name = $_POST['app_name'];
 }else{
@@ -40,10 +39,10 @@ if( isset($_POST['link']) && $_POST['link'] != ''){
 	echo json_encode($data);
 	exit;
 }
-*/
 
-$app_name = 'daxiangce1';
-$id = 1;
+
+// $app_name = 'daxiangce1';
+// $id = 1;
 $intro = 'share photo';
 $link = 'http://baidu.com?id=1';
 $connect = mysql_connect($config['mysql_host'], $config['mysql_username'], $config['mysql_passwd']);
